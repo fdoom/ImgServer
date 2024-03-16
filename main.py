393eecfdacc6f3c963dab364d26b56327ff390f6
fdoom <11jjklol@naver.com>
2024-03-16 19:31:17 +0900

    주소 통일 및 HTTP 메서드 별로 적용

--- main.py
+++ main.py
@@ -5,30 +5,38 @@ import os
 
 app = FastAPI()
 
-def save_uploaded_file(upload_folder: str, file: UploadFile):
-    os.makedirs(upload_folder, exist_ok=True)
-    with open(os.path.join(upload_folder, file.filename), "wb") as buffer:
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def is_valid_image_filename(filename: str) -> bool:
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
+
+def save_uploaded_file(path: str, file: UploadFile):
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, file.filename), "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-@app.post("/upload/")
-async def upload_file(upload_folder: str , file: UploadFile = File(...)):
+@app.post("/images/")
+async def upload_file(path: str , file: UploadFile = File(...)):
     try:
-        save_uploaded_file(upload_folder, file)
+        if not is_valid_image_filename(file.filename):
+            raise HTTPException(status_code=400, detail="Only images with extensions {} are allowed.".format(ALLOWED_IMAGE_EXTENSIONS))
+        
+        save_uploaded_file(path, file)
         return JSONResponse(content={"message": "File uploaded successfully"})
     except Exception as e:
         return JSONResponse(status_code=500, content={"message": f"An error occurred: {str(e)}"})
 
-@app.get("/images/{image_name}")
-def get_image(image_name: str, folder_path: str):
-    image_path = os.path.join(folder_path, image_name)
+@app.get("/images/")
+def get_image(image_name: str, path: str):
+    image_path = os.path.join(path, image_name)
     if os.path.exists(image_path):
         return FileResponse(image_path)
     else:
         raise HTTPException(status_code=404, detail="Image not found")
     
-@app.delete("/images/{image_name}")
-def delete_image(image_name: str, folder_path: str):
-    image_path = os.path.join(folder_path, image_name)
+@app.delete("/images/")
+def delete_image(image_name: str, path: str):
+    image_path = os.path.join(path, image_name)
     if os.path.exists(image_path):
         os.remove(image_path)
         return {"message": "Image deleted successfully"}
@@ -37,4 +45,4 @@ def delete_image(image_name: str, folder_path: str):
     
 @app.get("/")
 def read_root():
-    return {"Hello" : str(os.getcwd())}
+    return {"Hello" : "World!"}
